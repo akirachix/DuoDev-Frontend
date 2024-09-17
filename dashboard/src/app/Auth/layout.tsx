9aa48befa92c1fa90ebbbd135c4075f0a62b658b
mcgarretronald <mcgarretronald@gmail.com>
2024-09-17 23:24:02 +0300

    sign in ui

---
+++ dashboard/src/app/Auth/layout.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+export default function AuthLayout ({ children }: {children: React.ReactNode}) {
+    return(
+        <div>
+            <header><h1 className=""></h1></header>
+            <main>
+                {children}
+            </main>
+            <footer className=""><h1></h1></footer>
+        </div>
+    )
+    
+}
