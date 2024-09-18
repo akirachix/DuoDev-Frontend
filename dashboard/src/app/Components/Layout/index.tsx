1d348ff85ae2b0811ac650468e07ff07624066d3
HOSEWELL <hosewellkaranja@gmail.com>
2024-09-18 15:04:39 +0300

    pipeline fix

--- dashboard/src/app/Components/Layout/index.tsx
+++ dashboard/src/app/Components/Layout/index.tsx
@@ -1,7 +1,6 @@
 "use client"; 
 
 import React from 'react';
-
 import Sidebar from "../Sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
--- dashboard/src/app/page.tsx
+++ dashboard/src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Layout from "./Components/Layout";
 
 export default function Home() {
