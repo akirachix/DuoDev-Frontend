39f5835b2513cbfa479e4adf3cc85e3032bc07dc
mcgarretronald <mcgarretronald@gmail.com>
2024-09-20 13:19:15 +0300

    merge conflict fixed

--- dashboard/src/app/(public)/publicUser/marketplace/productListing/index.tsx
+++ dashboard/src/app/(public)/publicUser/marketplace/productListing/index.tsx
@@ -1,11 +1,5 @@
 "use client"
-import { useState, useEffect } from "react";
-
-
-
 export default function ProductsList (){
-    const [products, setProducts] = useState([]);
-
     return(
         <div>
             <h1>Products List</h1>
